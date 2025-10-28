2fbeb3dadb66704cfb1c1771b05acc8564450960
William Gomes <gomes.souza@live.com>
2025-10-28 19:45:43 +0100

    Adding code standarts and cliend response

--- src/server.cpp
+++ src/server.cpp
@@ -1,62 +1,102 @@
-#include <iostream>
-#include <cstdlib>
-#include <string>
-#include <cstring>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <string>
+
+// Function declarations
+std::string GetHttpStatusLine(int status_code);
+std::string GetBasicStatusMessage();
 
 int main(int argc, char **argv) {
   // Flush after every std::cout / std::cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-  
-  // You can use print statements as follows for debugging, they'll be visible when running tests.
+
+  // You can use print statements as follows for debugging, they'll be visible
+  // when running tests.
   std::cout << "Logs from your program will appear here!\n";
 
   // Uncomment this block to pass the first stage
   //
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (server_fd < 0) {
-   std::cerr << "Failed to create server socket\n";
-   return 1;
+    std::cerr << "Failed to create server socket\n";
+    return 1;
   }
-  
+
   // Since the tester restarts your program quite often, setting SO_REUSEADDR
   // ensures that we don't run into 'Address already in use' errors
   int reuse = 1;
-  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) <
+      0) {
     std::cerr << "setsockopt failed\n";
     return 1;
   }
-  
+
   struct sockaddr_in server_addr;
   server_addr.sin_family = AF_INET;
   server_addr.sin_addr.s_addr = INADDR_ANY;
   server_addr.sin_port = htons(4221);
-  
-  if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
+
+  if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) !=
+      0) {
     std::cerr << "Failed to bind to port 4221\n";
     return 1;
   }
-  
+
   int connection_backlog = 5;
   if (listen(server_fd, connection_backlog) != 0) {
     std::cerr << "listen failed\n";
     return 1;
   }
-  
+
   struct sockaddr_in client_addr;
   int client_addr_len = sizeof(client_addr);
-  
+
   std::cout << "Waiting for a client to connect...\n";
-  
-  accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+
+  int client_fd = accept(server_fd, (struct sockaddr *)&client_addr,
+         (socklen_t *)&client_addr_len);
+  if (client_fd < 0) {
+    std::cerr << "Failed to accept client connection\n";
+    return 1;
+  }
   std::cout << "Client connected\n";
-  
-  // close(server_fd);
+
+  //Send a basic HTTP response to the client
+  std::string http_response = GetBasicStatusMessage();
+  send(client_fd, http_response.c_str(), http_response.length(), 0);
+  std::cout << "HTTP response sent\n";
+
+  close(client_fd);
+  close(server_fd);
 
   return 0;
+}
+
+std::string GetHttpStatusLine(int status_code) {
+  switch (status_code) {
+    case 200:
+      return "HTTP/1.1 200 OK\r\n";
+    case 404:
+      return "HTTP/1.1 404 Not Found\r\n";
+    case 500:
+      return "HTTP/1.1 500 Internal Server Error\r\n";
+    default:
+      return "HTTP/1.1 500 Internal Server Error\r\n";
+  }
+}
+
+std::string GetBasicStatusMessage() {
+  return "HTTP/1.1 200 OK\r\n"
+         "Content-Length: 13\r\n"
+         "Content-Type: text/plain\r\n"
+         "\r\n"
+         "Hello, world!";
 }
